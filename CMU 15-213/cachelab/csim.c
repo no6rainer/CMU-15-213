85fc5dda236da24665e41a672bde3aed69424cff
no6rainer <xiepengrong@sjtu.edu.cn>
2025-11-12 17:53:50 +0800

    WIP: cachelab part 1

--- CMU 15-213/cachelab/csim.c
+++ CMU 15-213/cachelab/csim.c
@@ -1,7 +1,51 @@
 #include "cachelab.h"
 
-int main()
+#include <stdio.h>
+
+#include <getopt.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <stdbool.h>
+
+int main(int argc, char* argv[])
 {
+    int opt;
+    bool help = false;
+    bool verbose = false;
+    int set_bits = 0;
+    int asso = 0;
+    int block_bits = 0;
+    char* filename = NULL;
+
+    while ((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1) {
+        switch (opt) {
+            case 'h':
+                help = true;
+                break;
+            case 'v':
+                verbose = true;
+                break;
+            case 's':
+                set_bits = atoi(optarg);
+                break;
+            case 'E':
+                asso = atoi(optarg);
+                break;
+            case 'b':
+                block_bits = atoi(optarg);
+                break;
+            case 't':
+                filename = optarg;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n",
+                        argv[0]);
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
     printSummary(0, 0, 0);
     return 0;
 }
